321f61defe0594d7c92d1027413d17884064d1e7
wouterprommel <wouterprommel@gmail.com>
2024-10-31 17:31:21 +0100

    mc area

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,36 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-img_size = 1000
 
-x_axis = np.linspace(-2, 1, img_size)
-y_axis = np.linspace(-1.5, 1.5, img_size)
-image = np.zeros((img_size, img_size, 3)) # x, y, rgb
-max_iteration = 80
+def eval_point_mandelbrot(x, y):
+    max_iteration = 30
+    c = complex(x, y)
+    z = 0
+    iter = 0
+    bounded = True
+    itterating = True
+    while bounded and itterating:
+        bounded = abs(z) <= 2
+        itterating = iter < max_iteration
+        z = z*z + c
+        iter += 1
+    return (iter-1)/max_iteration # set red color (0-1)
 
-for j, x in enumerate(x_axis):
-    if j % 100 == 0: print(j)
-    for i, y in enumerate(y_axis):
-        c = complex(x, y)
-        z = 0
-        iter = 0
-        bounded = True
-        itterating = True
-        while bounded and itterating:
-            bounded = abs(z) <= 2
-            itterating = iter < max_iteration
-            z = z*z + c
-            iter += 1
-        image[i, j, 0] = (iter-1)/max_iteration # set red color (0-1)
+def mc_area(N):
+    # y from -1.5 to 1.5
+    # x form -2 to 1
+    X = -1.5 + 3*np.random.rand(N)
+    Y = -2 + 3*np.random.rand(N)
+    evaluations = []
+    for x, y in zip(X, Y):
+        eval = eval_point_mandelbrot(x, y) == 1
+        evaluations.append(eval)
 
-S = np.sum(image[:, :, 0])
-A = S/(img_size*img_size) * 3*3
-print(f"Area from pixel count: {A=}")
+    area = 9 * sum(evaluations) / len(evaluations)
+    print(f"Area from MC: {area=}")
 
-plt.imshow(image)
-plt.show()
+    return area
 
+def pixel_count_area(img_size = 1000):
 
+    x_axis = np.linspace(-2, 1, img_size)
+    y_axis = np.linspace(-1.5, 1.5, img_size)
+    image = np.zeros((img_size, img_size, 3)) # x, y, rgb
+    max_iteration = 80
+
+    for j, x in enumerate(x_axis):
+        if j % 100 == 0: print(j)
+        for i, y in enumerate(y_axis):
+            color = eval_point_mandelbrot(x, y)
+            image[i, j, 0] = color # set red color (0-1)
+
+    S = np.sum(image[:, :, 0])
+    A = S/(img_size*img_size) * 3*3
+    print(f"Area from pixel count: {A=}")
+
+    plt.imshow(image)
+    plt.show()
+
+mc_area(1000000)
 
 # single value itteration
 """ a = 0.28
