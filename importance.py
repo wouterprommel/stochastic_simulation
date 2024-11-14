4eec467db5cc1b76ef5af8cc28c75ae537dfdd26
fredloth <116084074+fredloth@users.noreply.github.com>
2024-11-14 20:28:15 +0100

    Correctly plots a contour for an area of interest

--- importance.py
+++ importance.py
@@ -13,13 +13,10 @@
 import mandelbrot
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.spatial import ConvexHull
-from scipy.stats import gaussian_kde
 
 def importance_space(img_size, max_iteration):
     x_axis = np.linspace(-2, 1, img_size)
     y_axis = np.linspace(-1.5, 1.5, img_size)
-    sample_space = []
     X, Y = np.meshgrid(x_axis, y_axis)
     Z = np.zeros(X.shape)
 
@@ -27,63 +24,23 @@ def importance_space(img_size, max_iteration):
         if j % 100 == 0: print(j)
         for i, y in enumerate(y_axis):
 
-            # To generate inferno map
-            val = mandelbrot.eval_point_mandelbrot(x, y, max_iteration)
-            Z[i, j] = val
+            Z[i, j] = mandelbrot.eval_point_mandelbrot(x, y, max_iteration)
 
-            if val == 1:
-                #print("Value!")
-                sample_space.append([x, y])
-            
-    x_vals = [point[0] for point in sample_space]
-    y_vals = [point[1] for point in sample_space]
-
-    #plt.scatter(x_vals, y_vals, s=0.5)
-
-    '''plt.imshow(Z, extent=(-2, 1, -1.5, 1.5), origin='lower', cmap='inferno')
-    
-    plt.colorbar()
-    plt.title('Sample space')
+    plt.contour(X, Y, Z, levels=np.linspace(0.95, 1.0, 5), colors='red')
     plt.xlabel('Real Part')
     plt.ylabel('Imaginary Part')
+    plt.title(f'Mandelbrot Set Approximation (Max Iterations = {max_iteration})')
+    plt.gca().set_aspect('equal')
     plt.show()
-    print("Z: ", Z)'''
 
-    if len(sample_space) > 2:
-       points = np.array(sample_space).T
-       kde = gaussian_kde(points)
-       kde_values = kde(np.vstack([X.ravel(), Y.ravel()]))
-       kde_values = kde_values.reshape(X.shape)
-       
-
-       plt.imshow(Z, extent=(-2, 1, -1.5, 1.5), origin='lower', cmap='inferno')
-       plt.colorbar()
-       plt.contour(X, Y, Y, kde_values, levels=[0.005], colors='red')
-       plt.title('Area of Interest')
-       plt.xlabel('Real Part')
-       plt.ylabel('Imaginary Part')
-       plt.show()
-
-
-       ''' hull = ConvexHull(sample_space)
-        hull_points = np.array(sample_space)[hull.vertices]
-
-        #plt.imshow(Z, extent=(-2, 1, -1.5, 1.5), origin='lower', cmap='inferno')
-        #plt.colorbar()
-        plt.scatter(*zip(*sample_space), s=0.5, color='blue')
-        hull_polygon = plt.Polygon(hull_points, fill=None, edgecolor='red', linewidth=2)
-        plt.gca().add_patch(hull_polygon)
-        plt.title('Convex Hull of Interest Area')
-        plt.xlabel('Real Part')
-        plt.ylabel('Imaginary Part')
-        plt.show()'''
-
-    return
 
 
 if __name__ == "__main__":
 
-    importance_space(1000, 5)
+    img_size = 500
+    max_iteration = 5
+
+    importance_space(img_size, max_iteration)
 
 
 
