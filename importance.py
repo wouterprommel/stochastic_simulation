41f4008267e9b4d1ccbc7b8b6db14413ec35ac29
fredloth <116084074+fredloth@users.noreply.github.com>
2024-11-14 20:52:31 +0100

    returns paths of the plt contourf

--- importance.py
+++ importance.py
@@ -14,6 +14,8 @@ import mandelbrot
 import matplotlib.pyplot as plt
 import numpy as np
 
+from matplotlib.path import Path
+
 def importance_space(img_size, max_iteration):
     x_axis = np.linspace(-2, 1, img_size)
     y_axis = np.linspace(-1.5, 1.5, img_size)
@@ -26,18 +28,36 @@ def importance_space(img_size, max_iteration):
 
             Z[i, j] = mandelbrot.eval_point_mandelbrot(x, y, max_iteration)
 
-    plt.contour(X, Y, Z, levels=np.linspace(0.95, 1.0, 5), colors='red')
+    area = plt.contourf(X, Y, Z, levels=np.linspace(0.95, 1.0, 5), colors='red')
     plt.xlabel('Real Part')
     plt.ylabel('Imaginary Part')
     plt.title(f'Mandelbrot Set Approximation (Max Iterations = {max_iteration})')
     plt.gca().set_aspect('equal')
     plt.show()
 
+    # Get the paths of the filled contour areas
+    paths = [Path(p.vertices) for collection in paths.collections for p in collection.get_paths()]
+
+    return paths
+
+
+
+    # Define the bounds of the area of interest
+    interest_area_indices = np.argwhere((Z >= 0.95) & (Z <= 1.0))
+    x_interest_min, x_interest_max = x_axis[interest_area_indices[:, 1].min()], x_axis[interest_area_indices[:, 1].max()]
+    y_interest_min, y_interest_max = y_axis[interest_area_indices[:, 0].min()], y_axis[interest_area_indices[:, 0].max()]
+
+    print("x_interest_min: ", x_interest_min)
+    print("x_interest_max: ", x_interest_max)
+    print("y_interest_min: ", y_interest_min)
+    print("y_interest_max: ", y_interest_max)
+
+
 
 
 if __name__ == "__main__":
 
-    img_size = 500
+    img_size = 50
     max_iteration = 5
 
     importance_space(img_size, max_iteration)
