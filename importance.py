5cdd45df9a4b20e5bc61df4e27632e1eb9aac8ba
fredloth <116084074+fredloth@users.noreply.github.com>
2024-11-14 19:41:54 +0100

    marks area of interest using convexx hull scipy

--- importance.py
+++ importance.py
@@ -14,6 +14,7 @@ import mandelbrot
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import ConvexHull
+from scipy.stats import gaussian_kde
 
 def importance_space(img_size, max_iteration):
     x_axis = np.linspace(-2, 1, img_size)
@@ -27,31 +28,62 @@ def importance_space(img_size, max_iteration):
         for i, y in enumerate(y_axis):
 
             # To generate inferno map
-            Z[i, j] = mandelbrot.eval_point_mandelbrot(x, y, max_iteration)
+            val = mandelbrot.eval_point_mandelbrot(x, y, max_iteration)
+            Z[i, j] = val
 
-            if eval == 1:
-                sample_space.append([j, i])
+            if val == 1:
+                #print("Value!")
+                sample_space.append([x, y])
             
     x_vals = [point[0] for point in sample_space]
     y_vals = [point[1] for point in sample_space]
 
     #plt.scatter(x_vals, y_vals, s=0.5)
 
-    plt.imshow(Z, extent=(-2, 1, -1.5, 1.5), origin='lower', cmap='inferno')
+    '''plt.imshow(Z, extent=(-2, 1, -1.5, 1.5), origin='lower', cmap='inferno')
     
     plt.colorbar()
     plt.title('Sample space')
     plt.xlabel('Real Part')
     plt.ylabel('Imaginary Part')
     plt.show()
-    print("Z: ", Z)
+    print("Z: ", Z)'''
+
+    if len(sample_space) > 2:
+       points = np.array(sample_space).T
+       kde = gaussian_kde(points)
+       kde_values = kde(np.vstack([X.ravel(), Y.ravel()]))
+       kde_values = kde_values.reshape(X.shape)
+       
+
+       plt.imshow(Z, extent=(-2, 1, -1.5, 1.5), origin='lower', cmap='inferno')
+       plt.colorbar()
+       plt.contour(X, Y, Y, kde_values, levels=[0.005], colors='red')
+       plt.title('Area of Interest')
+       plt.xlabel('Real Part')
+       plt.ylabel('Imaginary Part')
+       plt.show()
+
+
+       ''' hull = ConvexHull(sample_space)
+        hull_points = np.array(sample_space)[hull.vertices]
+
+        #plt.imshow(Z, extent=(-2, 1, -1.5, 1.5), origin='lower', cmap='inferno')
+        #plt.colorbar()
+        plt.scatter(*zip(*sample_space), s=0.5, color='blue')
+        hull_polygon = plt.Polygon(hull_points, fill=None, edgecolor='red', linewidth=2)
+        plt.gca().add_patch(hull_polygon)
+        plt.title('Convex Hull of Interest Area')
+        plt.xlabel('Real Part')
+        plt.ylabel('Imaginary Part')
+        plt.show()'''
 
     return
 
 
 if __name__ == "__main__":
 
-    importance_space(1000, 10)
+    importance_space(1000, 5)
 
 
 
