576fe50482e1f4720016fd8b23aed8f2a91aa083
fredloth <116084074+fredloth@users.noreply.github.com>
2024-11-18 18:21:15 +0100

    pep8

--- mandelbrot.py
+++ mandelbrot.py
@@ -6,35 +6,58 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 import Sample_methods
 import Masking
 
+
 def eval_point_mandelbrot(x, y, i):
-    '''
-    Determine if a point is in the mandelbrot set
-    return: range 0-1
-    '''
+    """
+    Determine if a point lies in the Mandelbrot set.
+
+    Parameters:
+    x (float): Real part of the complex number.
+    y (float): Imaginary part of the complex number.
+    i (int): Maximum number of iterations.
+
+    Returns: A float with normalized iteration count (range 0-1).
+    """
     max_iteration = i
     c = complex(x, y)
     z = 0
     iteration = 0
     bounded = True
     iterating = True
+
+    # Iterate over the point until it escapes.
     while bounded and iterating:
         bounded = abs(z) <= 2
         iterating = iteration < max_iteration
         z = z*z + c
         iteration += 1
+    
     return (iteration-1)/max_iteration # set red color (0-1)
 
+
 def mc_area(N, i, method='uniform', std=False):
-    '''
-    Compute the area of the Mandelbrot set using different sampling methods
-    return: float
-    '''
+    """
+    Compute the area of the Mandelbrot set using Monte Carlo methods.
+
+    Parameters:
+    N (int): Number of sample points.
+    i (int): Maximum number of iterations for the Mandelbrot evaluation.
+    method (str): Sampling method to use 
+        ('uniform', 'hypercube', 'orthogonal', 'adaptive').
+        Default is 'uniform'.
+    std (bool): Whether to return the standard deviation. 
+        Default is False.
+
+    Returns: Estimated area of the Mandelbrot set as a float. 
+        If `std=True`, returns a tuple (area, standard deviation).
+    """
     area_total = 9
     N = int(N)
+
+    # Generate sample points according to specified methods.
     if method == 'uniform':
         X = -1.5 + 3*np.random.rand(N)
         Y = -2 + 3*np.random.rand(N)
@@ -49,46 +72,59 @@ def mc_area(N, i, method='uniform', std=False):
     elif method == 'adaptive':
         img_size = 25
         i_space = 8
-        Z_boundary = 1
-        area_total, _, samples = Masking.adaptive(img_size, i_space, Z_boundary, N)
+        z_boundary = 1
+        area_total, samples = Masking.adaptive(img_size, i_space, z_boundary, N)
         N = len(samples)
 
+    # Evaluate to determine if a sample belongs to the Mandelbrot set.
     evaluations = []
     for x, y in samples:
         evaluation = eval_point_mandelbrot(x, y, i) == 1
         evaluations.append(evaluation)
 
+    # Calculate the estimated Mandelbrot set area and standard deviation.
     area = area_total * sum(evaluations) / len(evaluations)
-    std_value = area_total * np.std(evaluations, ddof=1)/np.sqrt(len(evaluations)) # sample variance
+    std_value = area_total * np.std(evaluations, ddof=1)/np.sqrt(len(evaluations))
 
     if std is True:
         return area, std_value
-    else:
-        return area
+
+    return area
 
 
 def pixel_count_area(img_size = 1000):
-    '''	
-    Computes the area of the Mandelbrot set using a pixel counting method
-    return: float
-    '''
+    """
+    Compute the area of the Mandelbrot set with a pixel counting method.
+
+    Parameters:
+    img_size (int): Resolution of the grid used for pixel counting. 
+        Default is 1000.
+
+    Returns: None, displays and saves the visualization of the 
+        Mandelbrot set.
+    """
     x_axis = np.linspace(-2, 1, img_size)
     y_axis = np.linspace(-1.5, 1.5, img_size)
     max_iteration = 80
 
-    # To generate inferno map
+    # Initialize an array to an generate inferno map for the Mandelbrot set.
     mandelbrot_set = np.zeros((img_size, img_size))
 
+    # Evaluate grid points in the array of the Mandelbrot set area
     for j, x in enumerate(x_axis):
         for i, y in enumerate(y_axis):
             mandelbrot_set[i, j] = eval_point_mandelbrot(x, y, max_iteration)
 
+    # Count the pixels inside the Mandelbrot set.
     S = np.sum(mandelbrot_set[:, :] == 1)
     A = S/(img_size*img_size) * 3*3
+
+
     print(f"Area from pixel count: {A=}")
     print("The relative error compared to the literature value is:",
           f"{np.abs(A - 1.5065)/1.5065*100}%.")
 
+    # Visualize the Mandelbrot set.
     plt.figure(figsize=(5.91/2, 3.6/2))
     plt.imshow(mandelbrot_set, extent=(-2, 1, -1.5, 1.5), cmap='inferno')
     colorbar = plt.colorbar()
@@ -100,43 +136,51 @@ def pixel_count_area(img_size = 1000):
     plt.savefig('Figures/Mandelbrot.pdf', bbox_inches='tight', format='pdf')
     plt.show()
 
+
 def timeing():
-    '''
-    Time to sample N amount of points for different sampling methods
-    '''
+    """
+    Measure the execution time for the different Monte Carlo sampling
+    methods and prints the elapsed time for each sampling method.
+    """
     N = int(1e4)
 
+    # Executes Pure random Monte Carlo sampling.
     t = time.time()
     print(mc_area(N, 80))
     print (np.round(time.time() - t, 3), 'sec elapsed for random mb')
 
+    # Executes Latin Hypercube Monte Carlo sampling.
     t = time.time()
     print(mc_area(N, 80, 'hypercube'))
     print (np.round(time.time() - t, 3), 'sec elapsed for hypercube mb')
 
+    # Executes Orthogonal Monte Carlo sampling.
     t = time.time()
     print(mc_area(N, 80, 'orthogonal'))
     print (np.round(time.time() - t, 3), 'sec elapsed for orthogonal mb')
 
+    # Executes custom Masking Monte Carlo sampling.
     t = time.time()
     print(mc_area(N, 80, 'adaptive'))
-    print (np.round(time.time() - t, 3), 'sec elapsed for adaptive orthogonal mb')
+    print (np.round(time.time() - t, 3), 'sec elapsed for masking orthogonal mb')
+
 
 def plot_samples():
-    ''' 
-    Plot sample points
-    '''
+    """Plots the sample points for different sampling methods. """
     N = int(25)
+
     samples = Sample_methods.hypercube(N)
-    samples2 = Sample_methods.orthogonal(N) # Set func to return samples first
+    samples2 = Sample_methods.orthogonal(N)
+
     plt.scatter(*zip(*samples))
     plt.scatter(*zip(*samples2))
     plt.show()
 
 
 if __name__ == "__main__":
-    # Use LaTex font for labels
+    # Run pixel counting and timeing analysis.
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
+
     pixel_count_area()
     timeing()
