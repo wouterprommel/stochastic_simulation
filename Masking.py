69c52fa1e7f6b5105575abb1c84888000595f45a
fredloth <116084074+fredloth@users.noreply.github.com>
2024-11-18 17:54:01 +0100

    pep8 masking

--- Masking.py
+++ Masking.py
@@ -1,51 +1,62 @@
-'''
-This module contains the functions for the adaptive sampling method.
-'''
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.ndimage import binary_fill_holes
 import mandelbrot
 
 
+def adaptive(img_size, i_space, z_boundary, sample_size):
+    """
+    Defines and determines a masked grid for sampling from the 
+    Mandelbrot set. Generates samples orthogonally from each cell in the 
+    masked grid.
 
-def adaptive(img_size, i_space, Z_boundary, sample_size):
-    '''
-    Takes samples orthogonally based on an defined area within which to accept samples.
-    return: float, np.array, list
-    '''
+    Parameters:
+    img_size (int): Number of pixels along one dimension to determine 
+        grid size.
+    i_space (int): Maximum number of iterations for evaluating the 
+        Mandelbrot set.
+    z_boundary (float): Threshold value to accept a pixel.
+    sample_size (int): Total number of samples to generate.
+
+    Returns:
+    area (float): Area of the masked grid occupied by valid pixels.
+    samples (list of tuples): List of sampled points (x, y) within the 
+        valid grid cells.
+    """
     x_min, x_max = -2, 1
     y_min, y_max = -1.5, 1.5
     x_axis = np.linspace(x_min, x_max, img_size)
     y_axis = np.linspace(y_min, y_max, img_size)
-
     X, Y = np.meshgrid(x_axis, y_axis)
+
+    # Create an array to store masked area as True/False.
     Z = np.zeros(X.shape)
 
+    # Evaluate pixels to determine if part of masked Mandelbrot area.
     for j, x in enumerate(x_axis):
         for i, y in enumerate(y_axis):
             Z[i, j] = mandelbrot.eval_point_mandelbrot(x, y, i_space)
 
-    pixels = Z >= Z_boundary
+    # Create boolean for points that meet threshhold value z_boundary.
+    pixels = Z >= z_boundary
     pixels_filled = pixels
 
-    # Compute the area occupied by pixels/boxes.
+    # Compute the area occupied by valid pixels.
     pixel_nr = np.sum(pixels_filled)
     dx = (x_max - x_min) / img_size
     dy = (y_max - y_min) / img_size
     pixel_area = abs(dx * dy)
     area = pixel_nr * pixel_area
 
-    # Set valid pixels and number of grids.
+    # Determine the number of samples to take per pixel.
     valid_pixels = np.argwhere(pixels_filled)
     num_valid_pixels = len(valid_pixels)
     samples_per_pixel = sample_size // num_valid_pixels
-
     subgrid_size = int(np.sqrt(samples_per_pixel))
 
     x_samples = []
     y_samples = []
-    samples = []
 
+    # Perform orthogonal sampling for every valid pixel.
     for _, (i, j) in enumerate(valid_pixels):
         x_bound = x_min + j * dx
         y_bound = y_min + i * dy
@@ -69,17 +80,17 @@ def adaptive(img_size, i_space, Z_boundary, sample_size):
             y_sample = np.random.uniform(y_edges[yi], y_edges[yi + 1])
             x_samples.append(x_sample)
             y_samples.append(y_sample)
-
+    
     samples = list(zip(x_samples, y_samples))
 
-    return area, pixels_filled, samples
+    return area, samples
 
 
-def adaptive_sample_space(area_filled, i_space, Z_boundary):
-    '''
-    Makes a plot of the adaptive grid sample space area and samples.
-    '''
-
+def adaptive_sample_space(area_filled):
+    """
+    Visualizes the adaptive grid sample space with valid areas 
+    highlighted.
+    """
     plt.figure(figsize=(12, 8))
     plt.imshow(area_filled, extent=(-2, 1, -1.5, 1.5), alpha=0.25)
     plt.tick_params(axis='x', labelsize=12)
@@ -87,5 +98,6 @@ def adaptive_sample_space(area_filled, i_space, Z_boundary):
     plt.xlabel('Real Part', fontsize=12)
     plt.ylabel('Imaginary Part', fontsize=12)
     plt.grid()
-    plt.savefig(f'Figures/Adaptive Sample Space.pdf', bbox_inches='tight', format='pdf')
+    plt.savefig(f'Figures/Adaptive Sample Space.pdf',
+                bbox_inches='tight', format='pdf')
     plt.show()
