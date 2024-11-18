befa5ea4f8b6508fef63965df25a8f6cc3e994f2
0Maan0 <maan.scipio@student.uva.nl>
2024-11-18 16:41:15 +0100

    pep8

--- Masking.py
+++ Masking.py
@@ -5,7 +5,10 @@ from scipy.ndimage import binary_fill_holes
 
 
 def adaptive(img_size, i_space, Z_boundary, sample_size):
-    """ Takes samples orthogonally based on an defined area within which to accept samples."""
+    '''
+    Takes samples orthogonally based on an defined area within which to accept samples.
+    return: float, np.array, list
+    '''
     x_min, x_max = -2, 1
     y_min, y_max = -1.5, 1.5
     x_axis = np.linspace(x_min, x_max, img_size)
@@ -28,7 +31,7 @@ def adaptive(img_size, i_space, Z_boundary, sample_size):
     pixel_area = abs(dx * dy)
     area = pixel_nr * pixel_area
 
-    # Set valid pixels and number of grids
+    # Set valid pixels and number of grids.
     valid_pixels = np.argwhere(pixels_filled)
     num_valid_pixels = len(valid_pixels)
     samples_per_pixel = sample_size // num_valid_pixels
@@ -63,14 +66,15 @@ def adaptive(img_size, i_space, Z_boundary, sample_size):
             x_samples.append(x_sample)
             y_samples.append(y_sample)
 
-    # sample size may differ, so change in mb
     samples = list(zip(x_samples, y_samples))
 
     return area, pixels_filled, samples
 
 
 def adaptive_sample_space(area_filled, i_space, Z_boundary):
-    """Makes a plot of the adaptive grid sample space area and samples."""
+    '''
+    Makes a plot of the adaptive grid sample space area and samples.
+    '''
 
     plt.figure(figsize=(12, 8))
     plt.imshow(area_filled, extent=(-2, 1, -1.5, 1.5), alpha=0.25)
@@ -78,8 +82,6 @@ def adaptive_sample_space(area_filled, i_space, Z_boundary):
     plt.tick_params(axis='y', labelsize=12)
     plt.xlabel('Real Part', fontsize=12)
     plt.ylabel('Imaginary Part', fontsize=12)
-    #plt.title(f'Sampled Points with Max Iterations = {i_space} and Z boundary = {Z_boundary}', fontsize=8)
-    #plt.gca().set_aspect('equal')   
     plt.grid()
     plt.savefig(f'Figures/Adaptive Sample Space.pdf', bbox_inches='tight', format='pdf')
     plt.show()
